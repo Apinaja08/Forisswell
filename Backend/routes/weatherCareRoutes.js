396bb77b9685e37179c15121f58abd8844ddaa65
Nikshan <nikshanps07@gmail.com>
2026-02-27 16:58:38 +0530

    weather js removed duplicate codes

--- Backend/routes/weatherCareRoutes.js
+++ Backend/routes/weatherCareRoutes.js
@@ -4,10 +4,9 @@ const router = express.Router();
 const { getWeatherByTree } = require("../controllers/weatherCareController");
 const { protect } = require("../middleware/auth");
 
-// All routes require authentication
+
 router.use(protect);
 
-// GET /api/weather-care/:treeId - Get weather data for a specific tree
 router.get("/:treeId", getWeatherByTree);
 
 module.exports = router;
